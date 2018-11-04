e3f12da8387b878c2cd9c5d11c52e34cd213c4c8
Dewit11 <davidkoller93@googlemail.com>
2018-11-04 22:10:06 +0100

    Small updates to Text extraction

--- AGB Crawler/AGB Crawler/TXT_Creator.py
+++ AGB Crawler/AGB Crawler/TXT_Creator.py
@@ -1,30 +1,38 @@
 import csv
 import requests
+import itertools
 
 from bs4 import BeautifulSoup as bs
 
 
 with open("List Generation/new_agb.csv" , newline="") as csv_file:
     list_reader = csv.reader(csv_file, delimiter=",")
-    next(list_reader, None)
+    # These variables and islice are just for my convenience to only create a few .txt at a time
+    startingPoint = 31
+    endPoint = None
+    partOfList = itertools.islice(list_reader, startingPoint, endPoint)
+    #next(list_reader, None)
     counter = 0
-    for row in list_reader:
+    for row in partOfList:
         if len(row) > 4:
             continue
-        if counter == 10:
+        if counter == 5:
             break
+        print("%s: %s" % (row[1], row[3][:-3]))
 
-        print("%s: %s" % (row[1], row[3]))
 
-        resp = requests.get(row[3])
+        resp = requests.get(row[3][:-3])
         soup = bs(resp.text, features="html.parser")
 
+        # rip it out
         for script in soup(["script", "style"]):
-            script.extract()  # rip it out
-
-        # get text
-        text = soup.body.get_text()
-
+            script.extract()
+        # for the rare Case some page doesn't have <body>
+        try:
+            # get text
+            text = soup.body.get_text()
+        except AttributeError:
+            continue
         # break into lines and remove leading and trailing space on each
         lines = (line.strip() for line in text.splitlines())
         # break multi-headlines into a line each
@@ -32,6 +40,6 @@ with open("List Generation/new_agb.csv" , newline="") as csv_file:
         # drop blank lines
         text = '\n'.join(chunk for chunk in chunks if chunk)
 
-        with open("Text Extraction/output2/%s.txt" % row[1], "w", encoding='utf-8') as file:
+        with open("Text Extraction/txt_output/%s.txt" % row[1], "w", encoding='utf-8') as file:
            file.write(text)
            counter += 1
