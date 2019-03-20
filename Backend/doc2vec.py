de567bb2ee47036bb599ba45e94012dd3bf89d35
Dewit11 <davidkoller93@googlemail.com>
2019-03-20 05:10:14 +0100

    doc2vec added

---
+++ Backend/doc2vec.py
@@ -0,0 +1,73 @@
+import server
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
+def get_tokenized_data_para():
+    docs = []
+    labels = []
+    for id in range(1,100):
+        #print("------------------", id)
+        agb = server.Agb.query.get(id)
+        for paragraph in agb.paragraphs:
+            token = paragraph.tokenText.split(',')
+            docs.append(token)
+            labels.append(paragraph.id)
+        #print(len(docs), len(labels))
+
+    return list(zip(docs, labels))
+
+def get_tokenized_data_clauses():
+    docs = []
+    labels = []
+    for id in range(1,100):
+        #print("------------------", id)
+        agb = server.Agb.query.get(id)
+        for clause in agb.clauses:
+            token = clause.tokenText.split(',')
+            docs.append(token)
+            labels.append(clause.id)
+        #print(len(docs), len(labels))
+
+    return list(zip(docs, labels))
+
+def build_model(data):
+    tagged_data = []
+    for i, doc in enumerate(data):
+        tagged_data.append(TaggedDocument(words=doc[0], tags=[doc[1]]) )
+    print(tagged_data[1])
+    print(len(tagged_data))
+
+    max_epochs = 100
+    vec_size = 1024
+    alpha = 0.025
+
+    model = Doc2Vec(vector_size=vec_size, alpha=alpha, min_alpha=0.00025, min_count=1, dm=1)
+    model.build_vocab(tagged_data)
+
+    for epoch in range(max_epochs):
+        print('iteration {0}'.format(epoch))
+        model.train(tagged_data,
+                    total_examples=model.corpus_count,
+                    epochs=model.iter)
+        # decrease the learning rate
+        model.alpha -= 0.0002
+        # fix the learning rate, no decay
+        model.min_alpha = model.alpha
+
+
+    print(model.docvecs['Grundwahrheit_6'])
+    model.save("d2v_c1024.model")
+
+if __name__ == '__main__':
+    poc = input("Enter p OR c: ")
+    data = []
+
+    if poc == "p": data = get_tokenized_data_para()
+    elif poc == "c": data = get_tokenized_data_clauses()
+
+    print(len(data))
+
+    build_model(data)
+    print("Done")
+
+
+
--- Backend/evaluation.py
+++ Backend/evaluation.py
@@ -5,10 +5,9 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+from gensim.models.doc2vec import Doc2Vec
 import time
+from multiprocessing import Pool
 
 import server
 from token_and_sim import convert_to_Array
@@ -23,10 +22,12 @@ def set_models():
     models.append(('SVM', SVC(gamma='auto')))
     return models
 
-def get_data(paraOrClause):
+def get_data(paraOrClause, with_Doc2Vec):
     model_data = []
     model_classes = []
+
     if paraOrClause == "para":
+        d2v_model = Doc2Vec.load("d2v_p1024_dm0.model")
         paraVectors = server.Vector.query.filter_by(clause_id = None).all()
         allParagraphs = server.Paragraph.query.all()
         print ("paraVectors", len(paraVectors))
@@ -34,6 +35,9 @@ def get_data(paraOrClause):
         data= list(zip(paraVectors, allParagraphs))
         for entry in data:
             arrayWordVector = convert_to_Array(entry[0].vector)
+            if with_Doc2Vec == True:
+                arrayWordVector.extend(d2v_model.docvecs[entry[1].id])
+                #print("neue Länge", len(arrayWordVector))
             model_data.append(arrayWordVector)
             model_classes.append(entry[1].trueState)
 
@@ -42,6 +46,7 @@ def get_data(paraOrClause):
         print("Model Classes", len(model_classes))
 
     elif paraOrClause == "clause":
+        d2v_model = Doc2Vec.load("d2v_c1024.model")
         clauseVectors = server.Vector.query.filter_by(paragraph_id=None).all()
         allClauses = server.Clause.query.all()
         print ("clauseVector", len(clauseVectors))
@@ -49,6 +54,9 @@ def get_data(paraOrClause):
         data= list(zip(clauseVectors, allClauses))
         for entry in data:
             arrayWordVector = convert_to_Array(entry[0].vector)
+            if with_Doc2Vec == True:
+                arrayWordVector.extend(d2v_model.docvecs[entry[1].id])
+                #print("neue Länge", len(arrayWordVector))
             model_data.append(arrayWordVector)
             model_classes.append(entry[1].trueState)
 
@@ -58,19 +66,12 @@ def get_data(paraOrClause):
 
     return [model_data, model_classes]
 
-if __name__ == '__main__':
-    poc = input("Enter p OR c: ")
-    t0 = time.time()
-    models = set_models()
-    data = []
-    if poc == "p": data = get_data("para")
-    elif poc == "c": data = get_data("clause")
-
+def create_cross_validation(models, data):
     seed = 7
     scoring = []
-    scoring.append('accuracy')
+    #scoring.append('accuracy')
     scoring.append('precision_weighted')
-    #scoring.append('recall_weighted')
+    scoring.append('recall_weighted')
     scoring.append('f1_weighted')
 
     for name, model in models:
@@ -80,6 +81,30 @@ if __name__ == '__main__':
             cv_results = model_selection.cross_val_score(model, data[0], data[1], cv=kfold, scoring=score)
             msg = "%s: %f (%f) for %s" % (name, cv_results.mean(), cv_results.std(), score)
             print(msg)
+
+if __name__ == '__main__':
+    poc = input("Enter p OR c: ")
+    t0 = time.time()
+    models = set_models()
+    data = []
+    if poc == "p": data = get_data("para", True)
+    elif poc == "c": data = get_data("clause", True)
+
+    pool = Pool()
+    result1 = pool.apply_async(create_cross_validation, [models[0:1], data])
+    result2 = pool.apply_async(create_cross_validation, [models[1:2], data])
+    result3 = pool.apply_async(create_cross_validation, [models[2:3], data])
+    result4 = pool.apply_async(create_cross_validation, [models[3:4], data])
+    result5 = pool.apply_async(create_cross_validation, [models[4:5], data])
+    result6 = pool.apply_async(create_cross_validation, [models[5:6], data])
+    answer1 = result1.get(timeout=5000)
+    answer2 = result2.get(timeout=5000)
+    answer3 = result3.get(timeout=5000)
+    answer4 = result4.get(timeout=5000)
+    answer5 = result5.get(timeout=5000)
+    answer6 = result6.get(timeout=5000)
+
+
 
     print("Final time:", time.time()- t0)
     print("Done")
