eb976b9fab59470e53d3f9f0fb73843e350b8297
Dewit11 <davidkoller93@googlemail.com>
2019-04-28 22:57:12 +0200

    Removed unused function

--- Backend/vector_Creation.py
+++ Backend/vector_Creation.py
@@ -1,38 +1,18 @@
 from elmoformanylangs import Embedder
 import numpy as np
+import time
 
 import server
-#from server import db
 
+def create_meanVector_cleanedText(id):
+    print("----------Vector Creation started----------")
 
-
-def create_SentenceVector_rawText(id):
     e = Embedder('..\\142')
     agb = server.Agb.query.get(id)
 
-    sentence = list(map(lambda clause : [clause.rawText] , agb.clauses))
-    result = e.sents2elmo(sentence)
-    vectorList = []
-    for i, ele in enumerate(result):
-        print (sentence[i][0])
-        print ("Satz " + str(i) +" : ", ele[0])
-        vectorList.append(ele[0])
-
-    for counter, clause in enumerate(agb.clauses):
-        #print ("Klausel :", clause.rawText)
-        newVector = ','.join(str(x) for x in vectorList[counter])
-        clause.meanVector = newVector
-        server.db.session.commit()
-
-def create_meanVector_cleanedText(id, e):
-    print("----------Vector Creation started----------")
-
-    #e = Embedder('..\\142')
-    agb = server.Agb.query.get(id)
-
-    # To create Vectors while loading the Embedder only once
+    ##### To create Vectors while loading the Embedder only once
     create_vectors_for = [agb.clauses, agb.paragraphs]
-    # Checking if we're currently working on clauses (True) or paragraphs(False)
+    ##### Checking if we're currently working on clauses (True) or paragraphs(False)
     flag_forClauses = True
 
     for clause_or_paragraph in create_vectors_for:
@@ -43,14 +23,14 @@ def create_meanVector_cleanedText(id, e):
         print("Anzahl Sätze in sentence", len(sentence))
         print("Anzahl Sätze in result", len(result))
 
-        #create meanVector here
+        ##### Create meanVector here
         for counter, wordVectors in enumerate(result):
             data = np.array(wordVectors)
             average = np.average(data, axis=0)
             vectorList.append(average)
 
         for counter, c_OR_p in enumerate(clause_or_paragraph):
-            #print ("Klausel :", clause.rawText)
+
             meanVector = ','.join(str(x) for x in vectorList[counter])
             if flag_forClauses == False:
                 new_meanVector = server.Vector(vector=meanVector, paragraph_id=c_OR_p.id, meanVector=True)
@@ -60,20 +40,22 @@ def create_meanVector_cleanedText(id, e):
                 new_meanVector = server.Vector(vector=meanVector, clause_id=c_OR_p.id, meanVector=True)
                 server.db.session.add(new_meanVector)
                 server.db.session.commit()
-                for vector in result[counter]:
-                    str_Vector = ','.join(str(x) for x in vector)
-                    new_vector = server.Vector(vector=str_Vector, clause_id=c_OR_p.id, meanVector=False)
-                    server.db.session.add(new_vector)
-                    server.db.session.commit()
+
+                ##### Uncomment the following code, if all Word Vectors should be stored on its own
+                ##### (Not Recommended due to extremly long loading times later on)
+                # for vector in result[counter]:
+                #     str_Vector = ','.join(str(x) for x in vector)
+                #     new_vector = server.Vector(vector=str_Vector, clause_id=c_OR_p.id, meanVector=False)
+                #     server.db.session.add(new_vector)
+                #     server.db.session.commit()
 
         print("----------Vector Creation for", flag_forClauses, "ended----------")
         flag_forClauses = False
 
 if __name__ == '__main__':
     id = input("Enter ID: ")
-    task = input("Choose Method: ")
-    if task == "raw" : create_SentenceVector_rawText(id)
-    elif task == "cle": create_meanVector_cleanedText(id)
+    t0 = time.time()
 
-    print("Code run (hopefully successfully)")
-    print ("Your last Input:", id)
+    create_meanVector_cleanedText(id)
+
+    print("Final time:", time.time() - t0)
